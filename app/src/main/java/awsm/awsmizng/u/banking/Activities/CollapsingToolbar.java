73e790839d669e26f249ede029541a52562bafbc
awsm1123 <jaayushmax@gmail.com>
2019-11-12 03:20:31 +0530

    collapsing toolbar

--- app/src/main/java/awsm/awsmizng/u/banking/Activities/CollapsingToolbar.java
+++ app/src/main/java/awsm/awsmizng/u/banking/Activities/CollapsingToolbar.java
@@ -1,13 +1,18 @@
 package awsm.awsmizng.u.banking.Activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import awsm.awsmizng.u.banking.R;
 
 public class CollapsingToolbar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collapsing_toolbar);
+        ActionBar bar = getSupportActionBar();
+        bar.hide();
     }
 }
